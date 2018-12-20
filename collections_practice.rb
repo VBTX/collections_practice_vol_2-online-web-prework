88a716f1a4ccb0e4c5f926c14e3db4ba0448e803
Victoria B <viktorija.bourassa@gmail.com>
2018-12-20 20:42:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
-# your code goes here
+def begins_with_r(array)
+    array.all? do |e|
+      e.start_with?("r")
+  end
+end
+
+def contain_a(array)
+  array.select do |item|
+    item.include?("a")
+end
+end
+
+def first_wa(array)
+  array.find do |elem|
+  if (elem.is_a? String) && (elem.start_with?("wa"))
+    return elem
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |elem|
+    (elem.is_a? String) == false
+  end
+end
+
+
+def count_elements(array)
+  count = 0
+  nArr = []
+  nObj = {}
+  array.each do |name|
+  count += array.count(name)
+  nObj[:name] = name
+  nObj[:count] = count
+  nArr.push(nObj)
+  return nArr
+end
+end
+
+
+def merge_data(keys, data)
+  keys.flatten(data)
+end
+
+
+def find_cool(array)
+end
+def organize_schools(array)
+  array.sort
+end
