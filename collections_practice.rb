40419898c5b82fe4a786b74a4a1412c3afae004b
Victoria B <viktorija.bourassa@gmail.com>
2018-12-20 21:19:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,12 +34,19 @@ def count_elements(array)
 
  def merge_data(keys, data)
     merged = []
-    keys.each {|i| data.first.map {|k,v| if i.values[0] == k then merged << i.merge(v) end}}
+    keys.each 
+    {|i| data.first.map 
+      {|k,v| if i.values[0] == k then merged << i.merge(v) end}
+      }
     merged
   end
 
    def find_cool(cool)
-      cool.select {|i| i.any? {|k,v| v == "cool"}}
+      cool.select do |i| 
+        i.any? do |k,v| 
+          v == "cool"
+        end
+      end
   end
 
 def organize_schools(array)
